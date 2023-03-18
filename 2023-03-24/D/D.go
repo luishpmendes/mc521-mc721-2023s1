6950ed7c97f31088df41b76dd2a285f732aa7891
Luis H. P. Mendes <luishpmendes@gmail.com>
2023-03-18 19:27:15 -0300

    CodeForces 1279C - Stack of Presents
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- 2023-03-24/D/D.go
+++ 2023-03-24/D/D.go
@@ -1,48 +1,42 @@
-// https://codeforces.com/problemset/problem/1760/C
+// https://codeforces.com/problemset/problem/1279/C
 
-// CodeForces 1760C - Advantage
+// CodeForces 1279C - Stack of Presents
 
-// There are n participants in a competition, participant i having a strength of si.
-// Every participant wonders how much of an advantage they have over the other best participant. In other words, each participant i wants to know the difference between si and sj, where j is the strongest participant in the competition, not counting i (a difference can be negative).
-// So, they ask you for your help! For each i (1≤i≤n) output the difference between si and the maximum strength of any participant other than participant i.
+// Santa has to send presents to the kids. He has a large stack of n presents, numbered from 1 to n; the topmost present has number a1, the next present is a2, and so on; the bottom present has number an. All numbers are distinct.
+// Santa has a list of m distinct presents he has to send: b1, b2, ..., bm. He will send them in the order they appear in the list.
+// To send a present, Santa has to find it in the stack by removing all presents above it, taking this present and returning all removed presents on top of the stack. So, if there are k presents above the present Santa wants to send, it takes him 2k+1 seconds to do it. Fortunately, Santa can speed the whole process up — when he returns the presents to the stack, he may reorder them as he wishes (only those which were above the present he wanted to take; the presents below cannot be affected in any way).
+// What is the minimum time required to send all of the presents, provided that Santa knows the whole list of presents he has to send and reorders the presents optimally? Santa cannot change the order of presents or interact with the stack of presents in any other way.
+// Your program has to answer t different test cases.
 
 // Input
-// The input consists of multiple test cases. The first line contains an integer t (1≤t≤1000) — the number of test cases. The descriptions of the test cases follow.
-// The first line of each test case contains an integer n (2≤n≤2⋅10^5) — the length of the array.
-// The following line contains n space-separated positive integers s1, s2, ..., sn (1≤si≤10^9) — the strengths of the participants.
-// It is guaranteed that the sum of n over all test cases does not exceed 2⋅10^5.
+// The first line contains one integer t (1≤t≤100) — the number of test cases.
+// Then the test cases follow, each represented by three lines.
+// The first line contains two integers n and m (1≤m≤n≤10^5) — the number of presents in the stack and the number of presents Santa wants to send, respectively.
+// The second line contains n integers a1, a2, ..., an (1≤ai≤n, all ai are unique) — the order of presents in the stack.
+// The third line contains m integers b1, b2, ..., bm (1≤bi≤n, all bi are unique) — the ordered list of presents Santa has to send.
+// The sum of n over all test cases does not exceed 10^5.
 
 // Output
-// For each test case, output n space-separated integers. For each i (1≤i≤n) output the difference between si and the maximum strength of any other participant.
+// For each test case print one integer — the minimum number of seconds which Santa has to spend sending presents, if he reorders the presents optimally each time he returns them into the stack.
 
 // Example
 // input
-// 5
-// 4
-// 4 7 3 5
 // 2
-// 1 2
-// 5
-// 1 2 3 4 5
-// 3
-// 4 9 4
-// 4
-// 4 4 4 4
+// 3 3
+// 3 1 2
+// 3 2 1
+// 7 2
+// 2 1 7 3 4 5 6
+// 3 1
 // output
-// -3 2 -4 -2
-// -1 1
-// -4 -3 -2 -1 1
-// -5 5 -5
-// 0 0 0 0
-
-// Note
-// For the first test case:
-//  • The first participant has a strength of 4 and the largest strength of a participant different from the first one is 7, so the answer for the first participant is 4−7=−3.
-//  • The second participant has a strength of 7 and the largest strength of a participant different from the second one is 5, so the answer for the second participant is 7−5=2.
-//  • The third participant has a strength of 3 and the largest strength of a participant different from the third one is 7, so the answer for the third participant is 3−7=−4.
-//  • The fourth participant has a strength of 5 and the largest strength of a participant different from the fourth one is 7, so the answer for the fourth participant is 5−7=−2.
+// 5
+// 8
 
 // Tutorial
+// At first let's precalculate array pos such that posai=i.
+// Now presume that we have to calculate answer for bi. Then there are two cases (let's denote lst=max1≤j<iposbj, initially lst=−1):
+//  1. if posbi>lst then we have to spend 1+2⋅(posbi−(i−1)) seconds on it (1 second on the gift bi, posbi−(i−1) seconds on removing gifts above and posbi−(i−1) seconds on pushing these gifts);
+//  2. if posbi<lst then we can reorder gifts by previous actions such that gift bi be on the top of stack. So we spend only 1 second on it.
 
 package main
 
@@ -50,37 +44,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 )
 
 func main() {
 	var reader = bufio.NewReader(os.Stdin)
 	var writer = bufio.NewWriter(os.Stdout)
-	var c, t, n, i int
-	var s, v []int
+
+	var c int
+	var t, n, m, i, lst, res uint
+	var a, b, pos []uint
 
 	for c, _ = fmt.Fscan(reader, &t); c == 1; c, _ = fmt.Fscan(reader, &t) {
 		for ; t > 0; t-- {
-			fmt.Fscan(reader, &n)
-			s = make([]int, n)
-			v = make([]int, n)
+			fmt.Fscan(reader, &n, &m)
+
+			a = make([]uint, n)
+			b = make([]uint, m)
+			pos = make([]uint, n+1)
 
 			for i = 0; i < n; i++ {
-				fmt.Fscan(reader, &s[i])
-				v[i] = s[i]
+				fmt.Fscan(reader, &a[i])
 			}
 
-			sort.Ints(v)
+			for i = 0; i < m; i++ {
+				fmt.Fscan(reader, &b[i])
+			}
 
 			for i = 0; i < n; i++ {
-				if s[i] != v[n-1] {
-					fmt.Fprint(writer, s[i]-v[n-1], " ")
-				} else {
-					fmt.Fprint(writer, s[i]-v[n-2], " ")
+				pos[a[i]] = i
+			}
+
+			lst = 0
+			res = m
+
+			for i = 0; i < m; i++ {
+				if pos[b[i]] > lst || i == 0 {
+					res += 2 * (pos[b[i]] - i)
+					lst = pos[b[i]]
 				}
 			}
 
-			fmt.Fprintln(writer)
+			fmt.Fprintln(writer, res)
 		}
 	}
 
